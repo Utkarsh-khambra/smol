cc25c7510eb40deb252febd3d3dbf3eed3ec9136
Utkarsh Khambra <utkarshqw@gmail.com>
2021-07-04 01:25:47 +0530

    basic lighting shading

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,27 @@ std::random_device rd;
 std::default_random_engine rng(rd());
 std::uniform_int_distribution<unsigned char> nums(0, 255);
 
+glm::vec3 light_dir(0, 0, -1);
+
 Image render_face(Model &model) {
   Image img(width, height, 3);
   for (auto i = 0ul; i < model.nfaces(); ++i) {
     auto f = model.face(i);
-    glm::vec2 screnn_cords[3];
+    glm::vec2 screen_cords[3];
+    glm::vec3 world_coords[3];
     for (auto j = 0; j < 3; ++j) {
       auto v = model.vert(f[j]);
-      screnn_cords[j] =
+      screen_cords[j] =
           glm::vec2((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+      world_coords[j] = v;
     }
-    draw_triangle(screnn_cords[0], screnn_cords[1], screnn_cords[2], img,
-                  {nums(rng), nums(rng), nums(rng)});
+    auto triangle_normal =
+        glm::normalize(glm::cross((world_coords[2] - world_coords[0]),
+                                  (world_coords[1] - world_coords[0])));
+    auto intensity = glm::dot(triangle_normal, light_dir);
+    if (intensity > 0)
+      draw_triangle(screen_cords[0], screen_cords[1], screen_cords[2], img,
+                    {intensity * 255, intensity * 255, intensity * 255});
   }
   return img;
 }
