4bd62f542cff4e63a4fb5050dc6e04a38a1c0da2
Utkarsh Khambra <utkarshqw@gmail.com>
2021-07-04 13:27:23 +0530

    added z buffering, with line sweeping

--- src/drawing.cpp
+++ src/drawing.cpp
@@ -4,8 +4,8 @@
 #include <cmath>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
-void draw_triangle(glm::ivec2 p, glm::ivec2 q, glm::ivec2 r, Image &img,
-                   glm::u8vec3 color) {
+void draw_triangle(glm::ivec2 p, glm::ivec2 q, glm::ivec2 r, glm::vec3 z_val,
+                   Image &img, glm::u8vec3 color, float *zbuffer) {
   if (p.y == q.y && p.y == r.y)
     return;
   if (p.y > q.y)
@@ -26,11 +26,19 @@ void draw_triangle(glm::ivec2 p, glm::ivec2 q, glm::ivec2 r, Image &img,
         second_half
             ? glm::ivec2{std::lerp(q.x, r.x, beta), std::lerp(q.y, r.y, beta)}
             : glm::ivec2{std::lerp(p.x, q.x, beta), std::lerp(p.y, q.y, beta)};
+    float a_z = std::lerp(z_val[0], z_val[2], alpha);
+    float b_z = second_half ? std::lerp(z_val[1], z_val[2], beta)
+                            : std::lerp(z_val[0], z_val[1], beta);
 
     if (a.x > b.x)
       std::swap(a, b);
-    for (int j = a.x; j <= b.x; ++j)
-      img.set(j, p.y + i, color);
+    for (int j = a.x; j <= b.x; ++j) {
+      auto z = std::lerp(a_z, b_z, static_cast<float>(j - a.x) / (b.x - a.x));
+      if (zbuffer[(p.y + i) * img.x() + j] < z) {
+        zbuffer[(p.y + i) * img.x() + j] = z;
+        img.set(j, p.y + i, color);
+      }
+    }
   }
 }
 
--- src/drawing.hpp
+++ src/drawing.hpp
@@ -2,7 +2,7 @@
 #define DRAWING_HPP
 #include "image.hpp"
 #include <glm/glm.hpp>
-void draw_triangle(glm::ivec2 p, glm::ivec2 q, glm::ivec2 r, Image &img,
-                   glm::u8vec3 color);
+void draw_triangle(glm::ivec2 p, glm::ivec2 q, glm::ivec2 r, glm::vec3 z_val,
+                   Image &img, glm::u8vec3 color, float *zbuffer);
 void line(glm::vec2 p, glm::vec2 q, Image &img, glm::u8vec3 color);
 #endif // DRAWING_HPP
